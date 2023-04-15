b0f6caac07951f9036ada02f754802537ce27c8e
ViniciusAcordiSoethe <soethe42@gmail.com>
2023-04-15 17:13:50 -0300

    Criando um retangolo que se movimenta

--- main.lua
+++ main.lua
@@ -1,9 +1,24 @@
-function love.draw()
-    love.graphics.print("Oi Besk o game está aqui!")
-end
 function love.load()
-    trevor = love.graphics.newImage("/images/trevor.jpg")
+    x = 200
+    y = 400
+    largura = 100
+    altura = 70
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("d") then
+        x = x + 100 * dt
     end
-    function love.draw()
-    love.graphics.draw(trevor)
+    if love.keyboard.isDown("a") then
+        x = x - 100 * dt
+    end
+    if love.keyboard.isDown("w") then
+        y = y - 100 * dt
+    end
+    if love.keyboard.isDown("s") then
+        y = y + 100 * dt
+    end
+end
+function love.draw()
+    love.graphics.rectangle("fill", x, y, largura, altura)
 end
